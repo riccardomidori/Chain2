e769aaa0ac60d2beff8875a4402bd29974b1d185
Riccardo <riccardo.sappa@midorisrl.eu>
2025-09-23 12:01:43 +0200

    20250923 - uv project

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from Utils import ModelVisualizer, VisualizationCallback, ModelTrainingTesting
 from CNN import CNNUpscaler
 import torch
 
+torch.set_float32_matmul_precision("medium")
+
+
 def train():
     batch_size = 128
 
